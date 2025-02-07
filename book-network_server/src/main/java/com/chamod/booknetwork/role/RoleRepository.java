b211d19a5f6323d34a59c0241205860738137d3a
chamod kavinda <kavindachamod100@gmail.com>
2025-02-07 21:58:23 +0530

    create Role Repository

---
+++ book-network_server/src/main/java/com/chamod/booknetwork/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.chamod.booknetwork.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+    Optional<Role> findByName(String role);
+}
