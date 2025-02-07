d3adab0c7d75664a322941f92fd61f31ca38b7fd
chamod kavinda <kavindachamod100@gmail.com>
2025-02-07 21:57:59 +0530

    create Role model

---
+++ book-network_server/src/main/java/com/chamod/booknetwork/role/Role.java
@@ -0,0 +1,40 @@
+package com.chamod.booknetwork.role;
+
+
+import com.chamod.booknetwork.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="user")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+    @CreatedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
