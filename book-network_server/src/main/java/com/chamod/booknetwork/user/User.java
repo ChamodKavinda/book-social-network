602baa5d3004f58f61d3a8d2a9fc9351c78b549f
chamod kavinda <kavindachamod100@gmail.com>
2025-02-09 13:58:36 +0530

    change table name

--- book-network_server/src/main/java/com/chamod/booknetwork/user/User.java
+++ book-network_server/src/main/java/com/chamod/booknetwork/user/User.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="user")
+@Table(name="users")
 @EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails, Principal {
     @Id
@@ -90,7 +90,7 @@ public class User implements UserDetails, Principal {
         return enabled;
     }
 
-    private String fullName(){
+    public String fullName(){
         return firstname + " " + lastname;
     }
 }
