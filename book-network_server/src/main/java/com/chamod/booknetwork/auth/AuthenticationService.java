1fbfab798d39df02e851e1da5dcd2ca720ec2b97
chamod kavinda <kavindachamod100@gmail.com>
2025-02-09 14:00:41 +0530

    implement send validation email method when user register

--- book-network_server/src/main/java/com/chamod/booknetwork/auth/AuthenticationService.java
+++ book-network_server/src/main/java/com/chamod/booknetwork/auth/AuthenticationService.java
@@ -2,7 +2,9 @@ package com.chamod.booknetwork.auth;
 
 
 import com.chamod.booknetwork.email.EmailService;
+import com.chamod.booknetwork.email.EmailTemplateName;
 import com.chamod.booknetwork.role.RoleRepository;
+import com.chamod.booknetwork.user.Token;
 import com.chamod.booknetwork.user.TokenRepository;
 import com.chamod.booknetwork.user.User;
 import com.chamod.booknetwork.user.UserRepository;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -40,10 +44,45 @@ public class AuthenticationService {
                 .roles(List.of(userRole))
                 .build();
         userRepository.save(user);
-
+        sendValidationEmail(user);
 
 
     }
 
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account actvation"
+        );
 
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i=0;i<length;i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return codeBuilder.toString();
+    }
 }
