aaa8e4e626b1dc119f801285dc4b648941915190
chamod kavinda <kavindachamod100@gmail.com>
2025-02-15 09:30:03 +0530

    create base class and inherit

--- book-network_server/src/main/java/com/chamod/booknetwork/book/Book.java
+++ book-network_server/src/main/java/com/chamod/booknetwork/book/Book.java
@@ -1,22 +1,22 @@
 package com.chamod.booknetwork.book;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import com.chamod.booknetwork.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Book {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
+public class Book extends BaseEntity {
+
     private String title;
     private String authorName;
     private String isbn;
@@ -25,16 +25,4 @@ public class Book {
     private boolean archived;
     private boolean shareable;
 
-    @CreatedDate
-    @Column(nullable = false,updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedOne;
-    @CreatedBy
-    @Column(nullable = false,updatable = false)
-    private Integer createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
