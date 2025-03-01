327055c459a0525548ac582ab2bfb4171a0172ed
chamod kavinda <kavindachamod100@gmail.com>
2025-03-01 23:27:49 +0530

    get current auditor class

---
+++ book-network_server/src/main/java/com/chamod/booknetwork/config/ApplicationAuditAware.java
@@ -0,0 +1,23 @@
+package com.chamod.booknetwork.config;
+
+import com.chamod.booknetwork.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ||
+            !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken){
+            return Optional.empty();
+        }
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
